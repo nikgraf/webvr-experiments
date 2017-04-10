1ef34ea4e796ab068c2d342cf7d7c2b2d8860015
Nik Graf <nik@nikgraf.com>
2017-04-10 10:02:30 +0200

    add animated model

---
+++ Animation/v3/index.vr.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import {
+  AppRegistry,
+  asset,
+  StyleSheet,
+  Pano,
+  View,
+  Model,
+  PointLight,
+} from 'react-vr';
+import RotatingCube from './components/RotatingCube.js';
+
+export default class HelloWorld extends React.Component {
+  render() {
+    return (
+      <View>
+        <RotatingCube />
+        <PointLight style={{color:'white', transform:[{translate : [0, 400, 700]}]}} />
+        <Pano source={asset('chess-world.jpg')}/>
+      </View>
+    );
+  }
+};
+
+AppRegistry.registerComponent('HelloWorld', () => HelloWorld);
---
+++ Animation/v3/rn-cli.config.js
@@ -0,0 +1,37 @@
+'use strict';
+
+var path = require('path');
+var blacklist = require('./node_modules/react-native/packager/blacklist');
+
+var config = {
+  getProjectRoots() {
+    return getRoots();
+  },
+
+  getBlacklistRE() {
+    return blacklist([
+    ]);
+  },
+
+  getAssetExts() {
+    return ['obj', 'mtl'];
+  },
+
+  getPlatforms() {
+    return ['vr'];
+  },
+
+  getProvidesModuleNodeModules() {
+    return ['react-native', 'react-vr'];
+  },
+};
+
+function getRoots() {
+  var root = process.env.REACT_NATIVE_APP_ROOT;
+  if (root) {
+    return [path.resolve(root)];
+  }
+  return [path.resolve(__dirname)];
+}
+
+module.exports = config;
