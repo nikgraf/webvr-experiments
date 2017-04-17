fad01b814f59712a1f9ecdb61466de5ca32725a3
Karl Horky <karl.horky@gmail.com>
2017-04-17 15:10:13 +0200

    Add first scaffolded ReactVR app

---
+++ Island/index.vr.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import {AppRegistry, asset, StyleSheet, Pano, Text, View} from 'react-vr';
+
+export default class Island extends React.Component {
+  render() {
+    return (
+      <View>
+        <Pano source={asset('chess-world.jpg')} />
+        <Text
+          style={{
+            backgroundColor: '#777879',
+            fontSize: 0.8,
+            layoutOrigin: [0.5, 0.5],
+            paddingLeft: 0.2,
+            paddingRight: 0.2,
+            textAlign: 'center',
+            textAlignVertical: 'center',
+            transform: [{translate: [0, 0, -3]}],
+          }}
+        >
+          hello
+        </Text>
+      </View>
+    );
+  }
+}
+
+AppRegistry.registerComponent('Island', () => Island);
